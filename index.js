5a0604e9d821ee027b4863a5ab9514254e0782d6
Elijah Kumi <elijah8512@yahoo.com>
2018-06-25 13:53:12 -0400

    Finished Day 1 Homework

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('It works!')
+const button = document.querySelector('button')
+const changeParagraph = function() {
+    const h1 = document.querySelector('h1')
+    h1.textContent = 'Kumi has a nice style'
+  }
+  button.addEventListener('click', changeParagraph)
