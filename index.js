12507514e0b3e72e2103d1d9d2ce5706b2fa28ba
Elijah Kumi <elijah8512@yahoo.com>
2018-06-25 13:57:37 -0400

    Finished Bonus Credit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('button')
 const changeParagraph = function() {
-    const h1 = document.querySelector('h1')
-    h1.textContent = 'Kumi has a nice style'
+    const h2 = document.querySelector('h2.H2')
+    h2.textContent = 'Kumi has a nice style'
   }
   button.addEventListener('click', changeParagraph)
