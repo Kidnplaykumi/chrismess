7412d5578945a64d7b550468026e8762d1719644
Elijah Kumi <elijah8512@yahoo.com>
2018-06-25 20:12:28 -0400

    Finished Super Mega Bonus Credit Hyper Fighting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form')
-var input =  document.getElementById("life").value;
+
 const changeParagraph = function(text) {
-    console.log("Hello")
+    // console.log("Hello")
     const h2 = document.querySelector('h2.H2')
     //sdocument.getElementById("life").value;
     h2.textContent = text 
