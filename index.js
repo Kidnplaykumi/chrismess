14969c8c6e23b49ee786caf133895ea898eae454
Elijah Kumi <elijah8512@yahoo.com>
2018-06-27 09:05:37 -0400

    Created a second form that added to the alreasdy existeing list

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const form = document.querySelector('form')
+const form = document.querySelector('form#form1')
+const form2 = document.querySelector('form#form2')
 
-const changeParagraph = function(text) {
-    // console.log("Hello")
-    const h2 = document.querySelector('h2.H2')
-    //sdocument.getElementById("life").value;
-    h2.textContent = text 
+const listOfMovie = function(ev) {
+    ev.preventDefault()
+    const f = ev.target
     
+    const life = f.life.value;
+    const item = document.createElement('li')
+    item.textContent = life
+    
+
+    const list = document.querySelector('#flicks')
+    list.appendChild(item)
+
+    f.reset() 
   }
-  form.addEventListener('submit', changeParagraph)
+
+
+const listOfArtist = function(ev){
+  ev.preventDefault()
+  const a = ev.target
+  
+  const life2 = a.life2.value;
+  
+  const item2 = document.createElement('li')
+  
+  item2.textContent = life2
+  console.log(item2)
+
+  const list2 = document.querySelector('#flicks2')
+  list2.appendChild(item2)
+
+  a.reset()
+}
+
+  
+  form.addEventListener('submit', listOfMovie)
+  form2.addEventListener('submit', listOfArtist)
